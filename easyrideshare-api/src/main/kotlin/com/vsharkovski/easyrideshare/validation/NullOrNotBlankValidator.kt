d0eaefc2f9a0594a679ca97b574205f198ef8302
Vladimir Sharkovski <vsarkovskii@gmail.com>
2022-08-18 00:19:22 +0200

    remove leftover files

--- easyrideshare-api/src/main/kotlin/com/vsharkovski/easyrideshare/validation/NullOrNotBlankValidator.kt
+++
@@ -1,9 +0,0 @@
-package com.vsharkovski.easyrideshare.validation
-
-import javax.validation.ConstraintValidator
-import javax.validation.ConstraintValidatorContext
-
-class NullOrNotBlankValidator : ConstraintValidator<NullOrNotBlank, String> {
-    override fun isValid(value: String?, context: ConstraintValidatorContext?): Boolean =
-        value == null || value.isNotBlank()
-}
