2f5b6a9372ca4a3ab4d9052ef661c87b045236f7
Vladimir Sharkovski <vsarkovskii@gmail.com>
2022-08-17 18:26:22 +0200

    small refactor

--- easyrideshare-api/src/main/kotlin/com/vsharkovski/easyrideshare/repository/UserRepository.kt
+++ easyrideshare-api/src/main/kotlin/com/vsharkovski/easyrideshare/repository/UserRepository.kt
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Repository
 @Repository
 interface UserRepository : JpaRepository<User, Long> {
     fun findByUsernameIgnoreCase(username: String): User?
+
     fun existsByUsernameIgnoreCase(username: String): Boolean
+
     fun existsByEmailIgnoreCase(email: String): Boolean
+
     fun findByUsernameContainingIgnoreCase(term: String): List<User>
 }
