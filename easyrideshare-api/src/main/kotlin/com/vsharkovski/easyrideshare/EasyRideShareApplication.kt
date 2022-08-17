949946608e73ed796ba5e662badf53efaaa25b22
Vladimir Sharkovski <vsarkovskii@gmail.com>
2022-08-17 14:12:23 +0200

    add initially generated projects

---
+++ easyrideshare-api/src/main/kotlin/com/vsharkovski/easyrideshare/EasyRideShareApplication.kt
@@ -0,0 +1,11 @@
+package com.vsharkovski.easyrideshare
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EasyRideShareApplication
+
+fun main(args: Array<String>) {
+	runApplication<EasyRideShareApplication>(*args)
+}
---
+++ easyrideshare-api/src/test/kotlin/com/vsharkovski/easyrideshare/EasyRideShareApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vsharkovski.easyrideshare
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EasyRideShareApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
